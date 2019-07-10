31757016ea4b7f5499fe37ed87c5855311304118
Kent C. Dodds <kent@doddsfamily.us>
2019-07-10 11:19:02 -0600

    update deps

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,4 +1,4 @@
-import 'jest-dom/extend-expect'
+import '@testing-library/jest-dom/extend-expect'
 import '@testing-library/react/cleanup-after-each'
 
 // this is just a little hack to silence a warning that we'll get until react
