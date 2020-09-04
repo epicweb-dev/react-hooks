82c92ae1b19435f76314afbfa2519d20289eb71d
Kent C. Dodds <me+github@kentcdodds.com>
2020-09-03 21:42:15 -0600

    update to the latest version of the workshop app

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import './styles.css'
 import codegen from 'codegen.macro'
-import './test/server'
 
 codegen`module.exports = require('@kentcdodds/react-workshop-app/codegen')`
--- src/test/server/index.js
+++
@@ -1,5 +0,0 @@
-if (process.env.NODE_ENV === 'test') {
-  module.exports = require('./test-server')
-} else {
-  module.exports = require('./dev-server')
-}
