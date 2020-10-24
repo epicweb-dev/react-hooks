7fc682d3465f29b2dfd39459728031147cae0b02
Dale Seo <dale.seo@gmail.com>
2020-10-23 21:30:35 -0400

    use getByRole instead of getByLabelText (#82)

--- src/__tests__/01.js
+++ src/__tests__/01.js
@@ -6,6 +6,6 @@ import App from '../final/01'
 
 test('typing a name shows a greeting', () => {
   render(<App />)
-  userEvent.type(screen.getByLabelText(/name/i), 'bob')
+  userEvent.type(screen.getByRole('textbox', {name: /name/i}), 'bob')
   screen.getByText(/hello.*bob/i)
 })
