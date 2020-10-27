a7fd993c299e59be489fb2af78067e450d30e984
Shawn Koski <skoski@artnet.com>
2020-10-27 12:00:09 -0400

    finished exercise 03, Lifting state

--- src/__tests__/03.extra-1.js
+++ src/__tests__/03.extra-1.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import {render, screen} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
-import App from '../final/03.extra-1'
-// import App from '../exercise/03'
+// import App from '../final/03.extra-1'
+import App from '../exercise/03'
 
 test('App works', () => {
   render(<App />)
