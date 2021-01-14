5a81c055f3b4a713a2d2ebc1cc47a232b063c998
Juan Segebre <juansegebre@gmail.com>
2021-01-14 21:41:04 +0100

    imported useLocalStorageState to avoid duplication

--- src/examples/local-state-key-change.js
+++ src/examples/local-state-key-change.js
@@ -3,39 +3,7 @@
 // http://localhost:3000/isolated/examples/local-state-key-change.js
 
 import * as React from 'react'
-
-function useLocalStorageState(
-  key,
-  defaultValue = '',
-  {serialize = JSON.stringify, deserialize = JSON.parse} = {},
-) {
-  const [state, setState] = React.useState(() => {
-    const valueInLocalStorage = window.localStorage.getItem(key)
-    if (valueInLocalStorage) {
-      // the try/catch is here in case the localStorage value was set before
-      // we had the serialization in place (like we do in previous extra credits)
-      try {
-        return deserialize(valueInLocalStorage)
-      } catch (error) {
-        window.localStorage.removeItem(key)
-      }
-    }
-    return typeof defaultValue === 'function' ? defaultValue() : defaultValue
-  })
-
-  const prevKeyRef = React.useRef(key)
-
-  React.useEffect(() => {
-    const prevKey = prevKeyRef.current
-    if (prevKey !== key) {
-      window.localStorage.removeItem(prevKey)
-    }
-    prevKeyRef.current = key
-    window.localStorage.setItem(key, serialize(state))
-  }, [key, state, serialize])
-
-  return [state, setState]
-}
+import { useLocalStorageState } from '../utils';
 
 function Greeting({initialName = ''}) {
   const [key, setKey] = React.useState('name');
