54d3ae29d451c63bb854144180707aee448af4e3
Ching-Ju Hsieh <chingjshieh@gmail.com>
2021-01-17 01:07:05 +0800

    feat: finish ex01 with useState hook

--- src/__tests__/01.js
+++ src/__tests__/01.js
@@ -1,8 +1,8 @@
 import * as React from 'react'
 import {render, screen} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
-import App from '../final/01'
-// import App from '../exercise/01'
+// import App from '../final/01'
+import App from '../exercise/01'
 
 test('typing a name shows a greeting', () => {
   render(<App />)
