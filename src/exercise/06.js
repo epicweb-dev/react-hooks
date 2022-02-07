7006da28f6495f738257bac2535f93a4acc53f4f
Prasanth Louis <prasanthlouis21@gmail.com>
2022-02-07 18:50:40 -0500

    Adding documentation on why we're setting Pokemon state to null before the HTTP call (#173)
    Co-authored-by: Kent C. Dodds <me+github@kentcdodds.com>

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -14,7 +14,8 @@ function PokemonInfo({pokemonName}) {
   // pokemon name changes.
   // 💰 DON'T FORGET THE DEPENDENCIES ARRAY!
   // 💰 if the pokemonName is falsy (an empty string) then don't bother making the request (exit early).
-  // 🐨 before calling `fetchPokemon`, clear the current pokemon state by setting it to null
+  // 🐨 before calling `fetchPokemon`, clear the current pokemon state by setting it to null.
+  // (This is to enable the loading state when switching between different pokemon.)
   // 💰 Use the `fetchPokemon` function to fetch a pokemon by its name:
   //   fetchPokemon('Pikachu').then(
   //     pokemonData => {/* update all the state here */},
