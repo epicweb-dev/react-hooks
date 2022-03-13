465d96b419329fb594e3f50ce125ab93c964f00c
linoyb <linoyb@codefuel.com>
2022-03-13 11:48:19 +0200

    email autofill

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -2,28 +2,31 @@
 // http://localhost:3000/isolated/exercise/01.js
 
 import * as React from 'react'
+import {useState} from "react";
 
-function Greeting() {
+function Greeting({initialName}) {
   // 💣 delete this variable declaration and replace it with a React.useState call
-  const name = ''
+  // const name = ''
+  const [name,setName] = useState(initialName)
 
   function handleChange(event) {
+    setName(event.target.value)
     // 🐨 update the name here based on event.target.value
   }
 
   return (
-    <div>
-      <form>
-        <label htmlFor="name">Name: </label>
-        <input onChange={handleChange} id="name" />
-      </form>
-      {name ? <strong>Hello {name}</strong> : 'Please type your name'}
-    </div>
+      <div>
+        <form>
+          <label htmlFor="name">Name: </label>
+          <input onChange={handleChange} value= {name} id="name" />
+        </form>
+        {name ? <strong>Hello {name}</strong> : 'Please type your name'}
+      </div>
   )
 }
 
 function App() {
-  return <Greeting />
+  return <Greeting initialName="Hola" />
 }
 
 export default App
--- src/exercise/02.js
+++ src/exercise/02.js
@@ -2,27 +2,40 @@
 // http://localhost:3000/isolated/exercise/02.js
 
 import * as React from 'react'
+function useLocalStorageState(key,initialName) {
+  const [state, setState] = React.useState(() =>(window.localStorage.getItem(key) ?? initialName))
 
+  React.useEffect(() => {
+    window.localStorage.setItem(key, state)
+  },[state])
+
+
+  return [state,setState];
+}
 function Greeting({initialName = ''}) {
   // 🐨 initialize the state to the value from localStorage
   // 💰 window.localStorage.getItem('name') ?? initialName
-  const [name, setName] = React.useState(initialName)
+  // const [name, setName] = React.useState(() =>(window.localStorage.getItem('name') ?? initialName))
 
   // 🐨 Here's where you'll use `React.useEffect`.
   // The callback should set the `name` in localStorage.
   // 💰 window.localStorage.setItem('name', name)
+  // React.useEffect(() => {
+  //   window.localStorage.setItem('name', name)
+  // },[name])
 
+  const [name,setName] =useLocalStorageState('name',initialName);
   function handleChange(event) {
     setName(event.target.value)
   }
   return (
-    <div>
-      <form>
-        <label htmlFor="name">Name: </label>
-        <input value={name} onChange={handleChange} id="name" />
-      </form>
-      {name ? <strong>Hello {name}</strong> : 'Please type your name'}
-    </div>
+      <div>
+        <form>
+          <label htmlFor="name">Name: </label>
+          <input value={name} onChange={handleChange} id="name" />
+        </form>
+        {name ? <strong>Hello {name}</strong> : 'Please type your name'}
+      </div>
   )
 }
 
