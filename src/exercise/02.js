e2f8e697070411228fd71fa06cd2f14990cb6c62
Stefano Ramirez <ramirez.stefano@gmail.com>
2023-02-20 15:59:31 -0600

    Ex2 with no extra credit done, implementing the React.useEffect to store the name value in localStorage

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -6,11 +6,22 @@ import * as React from 'react'
 function Greeting({initialName = ''}) {
   // 🐨 initialize the state to the value from localStorage
   // 💰 window.localStorage.getItem('name') ?? initialName
-  const [name, setName] = React.useState(initialName)
+
+  //change the argument to the local stored name.
+  //const [name, setName] = React.useState(initialName)
+  const [name, setName] = React.useState(window.localStorage.getItem('name') ?? initialName)
 
   // 🐨 Here's where you'll use `React.useEffect`.
+
+  //impleneting the React.useEffect to get the value from local storage and keep local
+  //storage updated as the 'name' is updated.
+React.useEffect(() => {
+  window.localStorage.setItem('name', name)
+})
+
   // The callback should set the `name` in localStorage.
   // 💰 window.localStorage.setItem('name', name)
+
 
   function handleChange(event) {
     setName(event.target.value)
