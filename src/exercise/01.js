b568af26c8262cf487517d959d56547e194f8655
Stefano Ramirez <ramirez.stefano@gmail.com>
2023-02-20 12:17:43 -0600

    Ex 1 done with all extra credit. Setting a value for the default useState and then updating it, also displaying the initial state as input that is then mmutable.

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -3,23 +3,30 @@
 
 import * as React from 'react'
 
-function Greeting() {
+//to use the prop it mast be passed in the greeting parameter.
+function Greeting({initialName}) {
   // 💣 delete this variable declaration and replace it with a React.useState call
   
   //changes de name declaraton to use the react use state and set a blank 
   //string for the initial state.
-  const [name, setName] = React.useState('')
+
+  //extra credit setting the default initial state to Kody in the greeting prop and calling it.
+
+  const [name, setName] = React.useState(initialName)
+
 
   function handleChange(event) {
     // 🐨 update the name here based on event.target.value
     setName(event.target.value)
   }
 
+  //for the extra credit the value prop is placed with the value of the use state to the name variable.
+  //uses the default state name 'Kody' but once there is a change the value is updated.
   return (
     <div>
       <form>
         <label htmlFor="name">Name: </label>
-        <input onChange={handleChange} id="name" />
+        <input onChange={handleChange} id="name" value={name} />
       </form>
       {name ? <strong>Hello {name}</strong> : 'Please type your name'}
     </div>
@@ -27,7 +34,7 @@ function Greeting() {
 }
 
 function App() {
-  return <Greeting />
+  return <Greeting initialName="Kody"/>
 }
 
 export default App
