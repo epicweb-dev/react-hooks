a4f5d3262c9a859683c0660976254aefe6a12b72
Stefano Ramirez <ramirez.stefano@gmail.com>
2023-02-28 13:17:18 -0600

    Ex6 with no extra credit done, use of useState and useEffect hooks to set the pokemon name and the dependecies on the name change.

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -6,12 +6,28 @@ import * as React from 'react'
 // fetchPokemon: the function we call to get the pokemon info
 // PokemonInfoFallback: the thing we show while we're loading the pokemon info
 // PokemonDataView: the stuff we use to display the pokemon info
+
+import {
+  fetchPokemon,
+  PokemonInfoFallback,
+  PokemonDataView
+} from '../pokemon'
+
 import {PokemonForm} from '../pokemon'
 
 function PokemonInfo({pokemonName}) {
   // 🐨 Have state for the pokemon (null)
+  const [pokemon, setPokemon] = React.useState(null)
   // 🐨 use React.useEffect where the callback should be called whenever the
   // pokemon name changes.
+  React.useEffect(()=>{
+    if(!pokemonName){return} //early return if no pokemon name.
+    setPokemon(null) //clearing the pokemon state by setting to null.
+
+    fetchPokemon(pokemonName).then(pokemon => setPokemon(pokemon))
+
+  }, [pokemonName]) //dependencies array set for pokemonName change.
+
   // 💰 DON'T FORGET THE DEPENDENCIES ARRAY!
   // 💰 if the pokemonName is falsy (an empty string) then don't bother making the request (exit early).
   // 🐨 before calling `fetchPokemon`, clear the current pokemon state by setting it to null.
@@ -20,13 +36,23 @@ function PokemonInfo({pokemonName}) {
   //   fetchPokemon('Pikachu').then(
   //     pokemonData => {/* update all the state here */},
   //   )
+  
+
   // 🐨 return the following things based on the `pokemon` state and `pokemonName` prop:
   //   1. no pokemonName: 'Submit a pokemon'
   //   2. pokemonName but no pokemon: <PokemonInfoFallback name={pokemonName} />
   //   3. pokemon: <PokemonDataView pokemon={pokemon} />
 
+  if(!pokemonName){
+    return 'Submit a pokemon'
+  } else if(!pokemon){
+    return <PokemonInfoFallback name={pokemonName} />
+  } else{
+    return <PokemonDataView pokemon={pokemon} />
+  }
+
   // 💣 remove this
-  return 'TODO'
+  //return 'TODO'
 }
 
 function App() {
