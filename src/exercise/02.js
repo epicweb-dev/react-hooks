d22690e0f514d2622d51e5e9cf84161fe3a850da
Varsha Thirumakil <Varsha.Thirumakil@vistaprint.com>
2023-05-01 12:52:23 -0400

    use Effect and custom hook

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -3,14 +3,20 @@
 
 import * as React from 'react'
 
-function Greeting({initialName = ''}) {
-  // 🐨 initialize the state to the value from localStorage
-  // 💰 window.localStorage.getItem('name') ?? initialName
-  const [name, setName] = React.useState(initialName)
+// Hooks
+function useLocalStorageState(key, defaultValue = '') {
+    // lazy state initialization
+    const [state, setState] = React.useState(() => window.localStorage.getItem(key) || defaultValue)
 
-  // 🐨 Here's where you'll use `React.useEffect`.
-  // The callback should set the `name` in localStorage.
-  // 💰 window.localStorage.setItem('name', name)
+    React.useEffect(() => {
+      window.localStorage.setItem(key, state)
+    }, [key, state])
+
+    return [state, setState]
+}
+
+function Greeting({initialName = ''}) {
+  const [name, setName] = useLocalStorageState('name', initialName)
 
   function handleChange(event) {
     setName(event.target.value)
@@ -27,7 +33,7 @@ function Greeting({initialName = ''}) {
 }
 
 function App() {
-  return <Greeting />
+  return <Greeting initialName='George' />
 }
 
 export default App
