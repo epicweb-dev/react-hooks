b0813973afcee8bd9bdf47922bcfa7eaccc309d0
navsi100rom <62949847+kutsen-rom@users.noreply.github.com>
2023-06-15 12:49:15 +0300

    Complete exercise 2.4

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -3,14 +3,33 @@
 
 import * as React from "react";
 
-function useLocalStorageState(key, defaultValue = "") {
-  const [state, setState] = React.useState(
-    () => window.localStorage.getItem(key) ?? defaultValue
-  );
+function useLocalStorageState(
+  key,
+  defaultValue = "",
+  { serialize = JSON.stringify, deserialize = JSON.parse } = {}
+) {
+  const [state, setState] = React.useState(() => {
+    const valueInLocalStorage = window.localStorage.getItem(key);
+    if (valueInLocalStorage) {
+      try {
+        return deserialize(valueInLocalStorage);
+      } catch (error) {
+        window.localStorage.removeItem(key);
+      }
+    }
+    return typeof defaultValue === "function" ? defaultValue() : defaultValue;
+  });
+
+  const prevKeyRef = React.useRef(key);
 
   React.useEffect(() => {
-    window.localStorage.setItem(key, state);
-  }, [key, state]);
+    const prevKey = prevKeyRef.current;
+    if (prevKey !== key) {
+      window.localStorage.removeItem(prevKey);
+    }
+    prevKeyRef.current = key;
+    window.localStorage.setItem(key, serialize(state));
+  }, [state, key, serialize]);
 
   return [state, setState];
 }
