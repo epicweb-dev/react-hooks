88cc230effe72e6f3f8659a378c1f58cd651ab51
navsi100rom <62949847+kutsen-rom@users.noreply.github.com>
2023-06-16 11:49:16 +0300

    Complete exercise 4.2

--- src/exercise/04.js
+++ src/exercise/04.js
@@ -2,18 +2,13 @@
 // http://localhost:3000/isolated/exercise/04.js
 
 import * as React from "react";
+import { useLocalStorageState } from "../utils";
 
 function Board() {
-  const [squares, setSquares] = React.useState(() =>
-    JSON.parse(window.localStorage.getItem("squares") ?? Array(9).fill(null))
-  );
+  const [squares, setSquares] = useLocalStorageState('squares', Array(9).fill(null));
   const nextValue = calculateNextValue(squares);
   const winner = calculateWinner(squares);
   const status = calculateStatus(winner, squares, nextValue);
-
-  React.useEffect(() => {
-    window.localStorage.setItem("squares", JSON.stringify(squares));
-  }, [squares]);
 
   function selectSquare(square) {
     if (winner || squares[square]) {
