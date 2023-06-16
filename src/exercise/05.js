934a52eb00d1baf986fbdfb0981cc189ac120f8c
kutsen-rom <62949847+kutsen-rom@users.noreply.github.com>
2023-06-16 15:51:42 +0300

    Complete exercise 5

--- src/exercise/05.js
+++ src/exercise/05.js
@@ -1,38 +1,29 @@
 // useRef and useEffect: DOM interaction
 // http://localhost:3000/isolated/exercise/05.js
 
-import * as React from 'react'
-// eslint-disable-next-line no-unused-vars
-import VanillaTilt from 'vanilla-tilt'
+import * as React from "react";
+import VanillaTilt from "vanilla-tilt";
 
-function Tilt({children}) {
-  // 🐨 create a ref here with React.useRef()
+function Tilt({ children }) {
+  const tiltRef = React.useRef();
 
-  // 🐨 add a `React.useEffect` callback here and use VanillaTilt to make your
-  // div look fancy.
-  // 💰 like this:
-  // const tiltNode = tiltRef.current
-  // VanillaTilt.init(tiltNode, {
-  //   max: 25,
-  //   speed: 400,
-  //   glare: true,
-  //   'max-glare': 0.5,
-  // })
-  //
-  // 💰 Don't forget to return a cleanup function. VanillaTilt.init will add an
-  // object to your DOM node to cleanup:
-  // `return () => tiltNode.vanillaTilt.destroy()`
-  //
-  // 💰 Don't forget to specify your effect's dependencies array! In our case
-  // we know that the tilt node will never change, so make it `[]`. Ask me about
-  // this for a more in depth explanation.
+  React.useEffect(() => {
+    const { current: tiltNode } = tiltRef;
+    const vanillaTiltOptions = {
+      max: 25,
+      speed: 400,
+      glare: true,
+      "max-glare": 0.5,
+    };
+    VanillaTilt.init(tiltNode, vanillaTiltOptions);
+    return () => tiltNode.vanillaTilt.destroy();
+  }, []);
 
-  // 🐨 add the `ref` prop to the `tilt-root` div here:
   return (
-    <div className="tilt-root">
+    <div ref={tiltRef} className="tilt-root">
       <div className="tilt-child">{children}</div>
     </div>
-  )
+  );
 }
 
 function App() {
@@ -40,7 +31,7 @@ function App() {
     <Tilt>
       <div className="totally-centered">vanilla-tilt.js</div>
     </Tilt>
-  )
+  );
 }
 
-export default App
+export default App;
