63b3e33e3a8329d4b4eb6056f3f895fc79aa9719
navsi100rom <62949847+kutsen-rom@users.noreply.github.com>
2023-06-16 08:16:08 +0300

    Complete exercise 3.0

--- src/exercise/03.js
+++ src/exercise/03.js
@@ -1,55 +1,43 @@
 // Lifting state
 // http://localhost:3000/isolated/exercise/03.js
 
-import * as React from 'react'
+import * as React from "react";
 
-function Name({name, onNameChange}) {
+function Name({ name, onNameChange }) {
   return (
     <div>
       <label htmlFor="name">Name: </label>
       <input id="name" value={name} onChange={onNameChange} />
     </div>
-  )
+  );
 }
 
-// 🐨 accept `animal` and `onAnimalChange` props to this component
-function FavoriteAnimal() {
-  // 💣 delete this, it's now managed by the App
-  const [animal, setAnimal] = React.useState('')
+function FavoriteAnimal({ animal, onAnimalChange }) {
   return (
     <div>
       <label htmlFor="animal">Favorite Animal: </label>
-      <input
-        id="animal"
-        value={animal}
-        onChange={event => setAnimal(event.target.value)}
-      />
+      <input id="animal" value={animal} onChange={onAnimalChange} />
     </div>
-  )
+  );
 }
 
-// 🐨 uncomment this
-// function Display({name, animal}) {
-//   return <div>{`Hey ${name}, your favorite animal is: ${animal}!`}</div>
-// }
-
-// 💣 remove this component in favor of the new one
-function Display({name}) {
-  return <div>{`Hey ${name}, you are great!`}</div>
+function Display({ name, animal }) {
+  return <div>{`Hey ${name}, your favorite animal is: ${animal}!`}</div>;
 }
 
 function App() {
-  // 🐨 add a useState for the animal
-  const [name, setName] = React.useState('')
+  const [animal, setAnimal] = React.useState("");
+  const [name, setName] = React.useState("");
   return (
     <form>
-      <Name name={name} onNameChange={event => setName(event.target.value)} />
-      {/* 🐨 pass the animal and onAnimalChange prop here (similar to the Name component above) */}
-      <FavoriteAnimal />
-      {/* 🐨 pass the animal prop here */}
-      <Display name={name} />
+      <Name name={name} onNameChange={(e) => setName(e.target.value)} />
+      <FavoriteAnimal
+        animal={animal}
+        onAnimalChange={(e) => setAnimal(e.target.value)}
+      />
+      <Display animal={animal} name={name} />
     </form>
-  )
+  );
 }
 
-export default App
+export default App;
