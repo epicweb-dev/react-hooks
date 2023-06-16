148e96d831fb1a1adb85138fe3f5bbcdda95f66f
navsi100rom <62949847+kutsen-rom@users.noreply.github.com>
2023-06-16 11:41:06 +0300

    Complete exercise 4.1

--- src/exercise/04.js
+++ src/exercise/04.js
@@ -4,10 +4,16 @@
 import * as React from "react";
 
 function Board() {
-  const [squares, setSquares] = React.useState(() => Array(9).fill(null));
+  const [squares, setSquares] = React.useState(() =>
+    JSON.parse(window.localStorage.getItem("squares") ?? Array(9).fill(null))
+  );
   const nextValue = calculateNextValue(squares);
   const winner = calculateWinner(squares);
   const status = calculateStatus(winner, squares, nextValue);
+
+  React.useEffect(() => {
+    window.localStorage.setItem("squares", JSON.stringify(squares));
+  }, [squares]);
 
   function selectSquare(square) {
     if (winner || squares[square]) {
