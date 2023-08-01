fe9b9258a0f683d955dbad031f90739ecb40816b
Brandon Wheeler <bwheeler2428@gmail.com>
2023-08-01 15:43:03 -0400

    Completed section 1 exercise

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -1,14 +1,13 @@
 // useState: greeting
 // http://localhost:3000/isolated/exercise/01.js
 
-import * as React from 'react'
+import React, {useState} from 'react'
 
 function Greeting() {
-  // 💣 delete this variable declaration and replace it with a React.useState call
-  const name = ''
+  const [name, setName] = useState('')
 
   function handleChange(event) {
-    // 🐨 update the name here based on event.target.value
+    setName(event.target.value)
   }
 
   return (
